dac9125c452dac385aac725200c5ce9c5d532a92
EvaristoAlves <evaristocarvalho34@gmail.com>
2021-04-05 22:47:39 -0300

    alterações

--- Aula4/AluraFlix.js
+++ Aula4/AluraFlix.js
@@ -1,4 +1,9 @@
-var filmes = ["Star Wars","Toy Story","Interstellar","ESqueceram de Mim","Marley e Eu"]
-for (var i = 0; i <= filmes.length;i++){
-    document.write("<img src =" + filmes[i] + ">")
+var listaNomefilmes = ["Toy Story","Star Wars","Interstellar"]
+
+
+var listaFilmes = ["https://m.media-amazon.com/images/M/MV5BMDU2ZWJlMjktMTRhMy00ZTA5LWEzNDgtYmNmZTEwZTViZWJkXkEyXkFqcGdeQXVyNDQ2OTk4MzI@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_SY317_CR0,0,214,317_AL_.jpg"]
+
+for (var i = 0; i < listaFilmes.length;i++){
+     document.write("<img src =" + listaFilmes[i] + ">")
+     document.write(listaNomefilmes[i])
 }
