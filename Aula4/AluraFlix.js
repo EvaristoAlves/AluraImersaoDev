df0d72a934712199f1186adb9eacae4c3ab6fceb
EvaristoAlves <evaristocarvalho34@gmail.com>
2021-04-05 23:02:03 -0300

    alteração

--- Aula4/AluraFlix.js
+++ Aula4/AluraFlix.js
@@ -1,4 +1,4 @@
-var listaNomefilmes = ["Toy Story","Star Wars","Interstellar"]
+var listaNomefilmes = ["Toy Story","Interstellar","Star Wars"]
 
 
 var listaFilmes = ["https://m.media-amazon.com/images/M/MV5BMDU2ZWJlMjktMTRhMy00ZTA5LWEzNDgtYmNmZTEwZTViZWJkXkEyXkFqcGdeQXVyNDQ2OTk4MzI@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_SY317_CR0,0,214,317_AL_.jpg"]
